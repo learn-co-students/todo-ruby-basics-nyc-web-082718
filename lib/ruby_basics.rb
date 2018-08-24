b381c574391774e47df1354e307cee7800c8f46f
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 06:18:30 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- puts num1 / num2
+  num1/num2
 end
 
-def assign_variable(value)
-  puts "#{variable}"
+def assign_variable(arg1)
+  variable = arg1
 end
 
-def argue(name)
-  puts name
+def argue(argument)
+  argument
 end
 
-def greeting(arg1, arg2)
- puts "#{arg1}, #{arg2}"
+def greeting(arg3, arg4)
+  puts "#{arg3}, #{arg4}"
 end
 
 def return_a_value()
-
+  "Nice"
 end
 
 def last_evaluated_value()
-
+  "learning"
+  "expert"
 end
 
-def pizza_party(pizza = "cheese")
-  puts "#{pizza}"
+def pizza_party(pizza="cheese")
+  pizza
 end
